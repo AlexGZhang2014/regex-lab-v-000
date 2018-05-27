2bcfc32ff4b88d3a1bd67e9c37a8d90f84b2affa
Alex Zhang <alexgzhang92@gmail.com>
2018-05-27 00:33:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,10 +7,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  =begin
+  
   new_text = text.split(' ')
   new_text.select {|word| word.match(/\Aun\w*ing\z/)}
-  =end
 end
 
 def words_five_letters_long(text)
